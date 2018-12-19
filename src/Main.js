2f11c54c682936cc834fe1e0c4538d6ff3d1ce6f
Kenny Jarnagin <jarnagink19@hanover.edu>
2018-12-18 23:46:07 -0500

    Create basic structure for passing properties

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Main.css';
+
+const Main = (props) => {
+    return (
+        <div>
+            <p>Testing Main.js</p>
+        </div>
+    )
+}
+
+export default Main
