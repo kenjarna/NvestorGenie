526d6446562ec48244331b6ae3314523e7d800f6
Kenny Jarnagin <jarnagink19@hanover.edu>
2018-12-18 23:52:24 -0500

    Create component for fetching stock information

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import './Main.css';
 
+import StockFetcher from './StockFetcher';
+
 const Main = (props) => {
     return (
         <div>
             <p>Testing Main.js</p>
+            <StockFetcher />
         </div>
     )
 }
---
+++ src/StockFetcher.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const StockFetcher = (props) => {
+    return (
+        <div>
+            <p>Testing StockFetcher (for IEXFinance API)</p>
+        </div>
+    )
+}
+
+export default StockFetcher
