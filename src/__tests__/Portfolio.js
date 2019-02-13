a7babc3ff42d16c840a5f82f306532555007d709
Kenny <jarnagink19@hanover.edu>
2019-02-12 23:04:33 -0500

    Trim the fat of tests and create new tests for Stock and Portfolio

--- src/__tests__/Portfolio.js
+++ src/__tests__/Portfolio.js
@@ -8,30 +8,87 @@ describe('Testing Portfolio Object functionality', function () {
     describe('Testing the creation of empty Portfolio object', function () {
         it('Test creation of a portfolio object', () => {
             expect(portfolio1.stockList).toEqual({});
-            expect(portfolio1.totalValue).toBe(0);
+            expect(portfolio1.totalValue).toEqual(0);
             expect(portfolio2.stockList).toEqual({});
-            expect(portfolio2.totalValue).toBe(0);
+            expect(portfolio2.totalValue).toEqual(0);
             expect(portfolio1).toEqual(portfolio2);
         });
 
     });
 
     describe('Testing Portfolio object methods', function () {
+        beforeEach(() => {
+            portfolio1 = new Portfolio();
+            portfolio2 = new Portfolio();
+        })
+
         it('Test addStock method of Portfolio object', () => {
             portfolio1.addStock('GOOGL', 100);
             expect(portfolio1.stockList).toEqual({
                 'GOOGL': googl
             });
-            expect(portfolio1.totalValue).toBe(0);
-
+            expect(portfolio1.totalValue).toEqual(0);
             expect(portfolio1).not.toEqual(portfolio2);
         });
         it('Test resetPortfolio method of Portfolio object', () => {
+            portfolio1.addStock('GOOGL', 100);
             portfolio1.resetPortfolio();
             expect(portfolio1.stockList).toEqual({});
-            expect(portfolio1.totalValue).toBe(0);
+            expect(portfolio1.totalValue).toEqual(0);
             expect(portfolio1).toEqual(portfolio2);
-        })
+        });
+
+        it('Test updatePortfolio method of Portfolio object on empty stock list', () => {
+            portfolio1.updatePortfolio();
+            expect(portfolio1).toEqual(portfolio2);
+        });
+        it('Test updatePortfolio method of Portfolio object on non-empty stock list', () => {
+            portfolio1.addStock('AMZN', 100);
+            portfolio1.addStock('GOOGL', 100);
+
+            portfolio1.updatePortfolio();
+            expect(portfolio1).not.toEqual(portfolio2);
+            expect(portfolio1.stockList).not.toEqual({});
+            expect(portfolio1.totalValue).not.toEqual(0);
+            //We should be sure that all the stats of a stock are not 
+            //their intially - assigned values
+            expect(portfolio1.stockList['AMZN'].companyName).not.toEqual('');
+            expect(portfolio1.stockList['AMZN'].latestTime).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].primaryExchange).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].latestPrice).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].PERatio).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].week52High).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].week52Low).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].ytdChange).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].closePrice).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].openPrice).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].beta).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].dividendRate).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].dividendYield).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].latestEPS).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].latestEPSDate).not.toEqual(null);
+            expect(portfolio1.stockList['AMZN'].investmentAmount).not.toEqual(null);
+            //Because there are two stocks in stockList, we need to make sure they have both been updated
+            expect(portfolio1.stockList['GOOGL'].companyName).not.toEqual('');
+            expect(portfolio1.stockList['GOOGL'].latestTime).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].primaryExchange).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].latestPrice).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].PERatio).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].week52High).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].week52Low).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].ytdChange).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].closePrice).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].openPrice).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].beta).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].dividendRate).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].dividendYield).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].latestEPS).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].latestEPSDate).not.toEqual(null);
+            expect(portfolio1.stockList['GOOGL'].investmentAmount).not.toEqual(null);
+
+            expect(portfolio1.stockList['AMZN']).not.toEqual(portfolio1.stockList['GOOGL']);
+
+        });
     });
 
 });
--- src/__tests__/Stock.js
+++ src/__tests__/Stock.js
@@ -2,51 +2,28 @@ import Stock from '../Stock';
 
 describe("Testing Stock Object functionality", function () {
     let amzn = new Stock('AMZN', 100);
-    let aapl = new Stock('AAPL', 103);
     describe('Testing creation of empty Stock object', function () {
         it('Test create Amazon Stock Object', () => {
-            expect(amzn.ticker).toBe('AMZN');
-            expect(amzn.numShares).toBe(100);
-            expect(amzn.companyName).toBe('');
-            expect(amzn.latestTime).toBe(0);
-            expect(amzn.primaryExchange).toBe(0);
-            expect(amzn.PERatio).toBe(0);
-            expect(amzn.week52High).toBe(0);
-            expect(amzn.week52Low).toBe(0);
-            expect(amzn.ytdChange).toBe(0);
-            expect(amzn.closePrice).toBe(0);
-            expect(amzn.openPrice).toBe(0);
-            expect(amzn.beta).toBe(0);
-            expect(amzn.dividendRate).toBe(0);
-            expect(amzn.dividendYield).toBe(0);
-            expect(amzn.latestEPS).toBe(0);
-            expect(amzn.latestEPSDate).toBe(0);
-            expect(amzn.investmentAmount).toBe(0);
-            expect(amzn.latestPrice).toBe(0);
+            expect(amzn.ticker).toEqual('AMZN');
+            expect(amzn.numShares).toEqual(100);
+            expect(amzn.companyName).toEqual('');
+            expect(amzn.latestTime).toEqual(null);
+            expect(amzn.primaryExchange).toEqual(null);
+            expect(amzn.PERatio).toEqual(null);
+            expect(amzn.week52High).toEqual(null);
+            expect(amzn.week52Low).toEqual(null);
+            expect(amzn.ytdChange).toEqual(null);
+            expect(amzn.closePrice).toEqual(null);
+            expect(amzn.openPrice).toEqual(null);
+            expect(amzn.beta).toEqual(null);
+            expect(amzn.dividendRate).toEqual(null);
+            expect(amzn.dividendYield).toEqual(null);
+            expect(amzn.latestEPS).toEqual(null);
+            expect(amzn.latestEPSDate).toEqual(null);
+            expect(amzn.investmentAmount).toEqual(null);
+            expect(amzn.latestPrice).toEqual(null);
         });
-        it('Test create apple stock object', () => {
-            expect(aapl.ticker).toBe('AAPL');
-            expect(aapl.numShares).toBe(103);
-            expect(aapl.companyName).toBe('');
-            expect(aapl.latestTime).toBe(0);
-            expect(aapl.primaryExchange).toBe(0);
-            expect(aapl.PERatio).toBe(0);
-            expect(aapl.week52High).toBe(0);
-            expect(aapl.week52Low).toBe(0);
-            expect(aapl.ytdChange).toBe(0);
-            expect(aapl.closePrice).toBe(0);
-            expect(aapl.openPrice).toBe(0);
-            expect(aapl.beta).toBe(0);
-            expect(aapl.dividendRate).toBe(0);
-            expect(aapl.dividendYield).toBe(0);
-            expect(aapl.latestEPS).toBe(0);
-            expect(aapl.latestEPSDate).toBe(0);
-            expect(aapl.investmentAmount).toBe(0);
-            expect(aapl.latestPrice).toBe(0);
-        });
-        it('Test inequality of Amazon and Apple Stock objects', () => {
-            expect(aapl).not.toBe(amzn);
-        });
+
     });
 
     describe('Testing Stock object methods', function () {
@@ -64,36 +41,19 @@ describe("Testing Stock Object functionality", function () {
                 'open': 1500
             };
             amzn.setBasicStats(quote);
-            aapl.setBasicStats(quote);
-            //Tests for Amazon
-            expect(amzn.ticker).toBe('AMZN');
-            expect(amzn.numShares).toBe(100);
-            expect(amzn.companyName).toBe('Amazon');
-            expect(amzn.latestTime).toBe('2/10/2019');
-            expect(amzn.primaryExchange).toBe('NASDAQ');
-            expect(amzn.week52High).toBe(1905);
-            expect(amzn.week52Low).toBe(1203);
-            expect(amzn.ytdChange).toBe(23);
-            expect(amzn.closePrice).toBe(1548);
-            expect(amzn.openPrice).toBe(1500);
-            expect(amzn.investmentAmount).toBe(176000);
-            expect(amzn.latestPrice).toBe(1760);
 
-            //Tests for Apple
-            expect(aapl.ticker).toBe('AAPL');
-            expect(aapl.numShares).toBe(103);
-            expect(aapl.companyName).toBe('Amazon');
-            expect(aapl.latestTime).toBe('2/10/2019');
-            expect(aapl.primaryExchange).toBe('NASDAQ');
-            expect(aapl.week52High).toBe(1905);
-            expect(aapl.week52Low).toBe(1203);
-            expect(aapl.ytdChange).toBe(23);
-            expect(aapl.closePrice).toBe(1548);
-            expect(aapl.openPrice).toBe(1500);
-            expect(aapl.investmentAmount).toBe(181280);
-            expect(aapl.latestPrice).toBe(1760);
-
-            expect(aapl).not.toBe(amzn);
+            expect(amzn.ticker).toEqual('AMZN');
+            expect(amzn.numShares).toEqual(100);
+            expect(amzn.companyName).toEqual('Amazon');
+            expect(amzn.latestTime).toEqual('2/10/2019');
+            expect(amzn.primaryExchange).toEqual('NASDAQ');
+            expect(amzn.week52High).toEqual(1905);
+            expect(amzn.week52Low).toEqual(1203);
+            expect(amzn.ytdChange).toEqual(23);
+            expect(amzn.closePrice).toEqual(1548);
+            expect(amzn.openPrice).toEqual(1500);
+            expect(amzn.investmentAmount).toEqual(176000);
+            expect(amzn.latestPrice).toEqual(1760);
 
         });
         it('Testing setStockInfo method of Stock Object', () => {
@@ -105,48 +65,30 @@ describe("Testing Stock Object functionality", function () {
                 'latestEPSDate': '2/10/19'
             };
             amzn.setStockInfo(stats);
-            aapl.setStockInfo(stats);
-
-            expect(aapl).not.toBe(amzn);
-            //Be sure the previously set stats using setBasicStats did NOT change for Apple Stock object
-            expect(aapl.ticker).toBe('AAPL');
-            expect(aapl.numShares).toBe(103);
-            expect(aapl.companyName).toBe('Amazon');
-            expect(aapl.latestTime).toBe('2/10/2019');
-            expect(aapl.primaryExchange).toBe('NASDAQ');
-            expect(aapl.week52High).toBe(1905);
-            expect(aapl.week52Low).toBe(1203);
-            expect(aapl.ytdChange).toBe(23);
-            expect(aapl.closePrice).toBe(1548);
-            expect(aapl.openPrice).toBe(1500);
-            expect(aapl.investmentAmount).toBe(181280);
-            expect(aapl.latestPrice).toBe(1760);
-            //Check that the new stats have been set properly for Apple Stock object
-            expect(aapl.beta).toBe(54);
-            expect(aapl.dividendYield).toBe(15.322);
-            expect(aapl.dividendRate).toBe(14.065);
-            expect(aapl.latestEPS).toBe(1.86);
-            expect(aapl.latestEPSDate).toBe('2/10/19');
 
             //Be sure the previously set stats using setBasicStats did NOT change for Amazon Stock object
-            expect(amzn.ticker).toBe('AMZN');
-            expect(amzn.numShares).toBe(100);
-            expect(amzn.companyName).toBe('Amazon');
-            expect(amzn.latestTime).toBe('2/10/2019');
-            expect(amzn.primaryExchange).toBe('NASDAQ');
-            expect(amzn.week52High).toBe(1905);
-            expect(amzn.week52Low).toBe(1203);
-            expect(amzn.ytdChange).toBe(23);
-            expect(amzn.closePrice).toBe(1548);
-            expect(amzn.openPrice).toBe(1500);
-            expect(amzn.investmentAmount).toBe(176000);
-            expect(amzn.latestPrice).toBe(1760);
+            expect(amzn.ticker).toEqual('AMZN');
+            expect(amzn.numShares).toEqual(100);
+            expect(amzn.companyName).toEqual('Amazon');
+            expect(amzn.latestTime).toEqual('2/10/2019');
+            expect(amzn.primaryExchange).toEqual('NASDAQ');
+            expect(amzn.week52High).toEqual(1905);
+            expect(amzn.week52Low).toEqual(1203);
+            expect(amzn.ytdChange).toEqual(23);
+            expect(amzn.closePrice).toEqual(1548);
+            expect(amzn.openPrice).toEqual(1500);
+            expect(amzn.investmentAmount).toEqual(176000);
+            expect(amzn.latestPrice).toEqual(1760);
             //Check that the new stats have been set properly for Amazon Stock object
-            expect(amzn.beta).toBe(54);
-            expect(amzn.dividendYield).toBe(15.322);
-            expect(amzn.dividendRate).toBe(14.065);
-            expect(amzn.latestEPS).toBe(1.86);
-            expect(amzn.latestEPSDate).toBe('2/10/19');
+            expect(amzn.beta).toEqual(54);
+            expect(amzn.dividendYield).toEqual(15.322);
+            expect(amzn.dividendRate).toEqual(14.065);
+            expect(amzn.latestEPS).toEqual(1.86);
+            expect(amzn.latestEPSDate).toEqual('2/10/19');
+        });
+        it('Test buildLink method of Stock object', () => {
+            expect(amzn.buildLink('quote', 'AMZN')).toEqual('https://api.iextrading.com/1.0/stock/AMZN/batch?types=quote');
+            expect(amzn.buildLink('stats', 'AMZN')).toEqual('https://api.iextrading.com/1.0/stock/AMZN/stats');
         });
     });
 
