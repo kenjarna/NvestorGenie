ef4ee11be8e2319f925843996d459b3dd7897252
Kenny Jarnagin <jarnagink19@hanover.edu>
2019-02-26 01:59:43 -0500

    Add a sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Sidebar.css';
+
+const Sidebar = () => {
+    return(
+        <div className="Sidebar">
+            <h3>Sidebar</h3>
+        </div>
+    )
+}
+
+export default Sidebar;
