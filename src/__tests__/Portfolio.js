9630f500c08b4b1c7e3bca58b4e2cdf8af0b4814
Kenny Jarnagin <jarnagink19@hanover.edu>
2019-04-02 04:41:03 -0400

    Simplify portfolio tests and test for new functionality Ref #8

--- src/__tests__/Portfolio.js
+++ src/__tests__/Portfolio.js
@@ -2,9 +2,14 @@ import Portfolio from '../LogicComponents/Portfolio';
 import Stock from '../LogicComponents/Stock';
 
 describe('Testing Portfolio Object functionality', function () {
-    let portfolio1 = new Portfolio();
-    let portfolio2 = new Portfolio();
-    let googl = new Stock('GOOGL', 100);
+    let portfolio1;
+    let portfolio2;
+    let GOOGL = new Stock('GOOGL', 100);
+
+    beforeEach(() => {
+        portfolio1 = new Portfolio();
+        portfolio2 = new Portfolio();
+    })
     describe('Testing the creation of empty Portfolio object', function () {
         it('Test creation of a portfolio object', () => {
             expect(portfolio1.stockList).toEqual({});
@@ -18,15 +23,11 @@ describe('Testing Portfolio Object functionality', function () {
     });
 
     describe('Testing Portfolio object methods', function () {
-        beforeEach(() => {
-            portfolio1 = new Portfolio();
-            portfolio2 = new Portfolio();
-        })
 
         it('Test addStock method of Portfolio object', () => {
             portfolio1.addStock('GOOGL', 100);
             expect(portfolio1.stockList).toEqual({
-                'GOOGL': googl
+                'GOOGL': GOOGL
             });
             expect(portfolio1.totalValue).toEqual(0);
             expect(portfolio1).not.toEqual(portfolio2);
@@ -41,59 +42,46 @@ describe('Testing Portfolio Object functionality', function () {
             expect(portfolio1.comments).toEqual("");
 
         });
-
         it('Test updatePortfolio method of Portfolio object on empty stock list', () => {
             portfolio1.updatePortfolio();
             expect(portfolio1.stockList).toEqual({});
             expect(portfolio1.totalValue).toEqual(0);
             expect(portfolio1.id).toEqual(6);
         });
-        it('Test updatePortfolio method of Portfolio object on non-empty stock list', () => {
-            portfolio1.addStock('AMZN', 100);
-            portfolio1.addStock('GOOGL', 100);
+        it('Test async updatePortfolio method on non-empty stock list', async ()=>{
+            portfolio1.addStock('AMZN',100);
+            portfolio1.addStock('GOOGL',53);
+            const stocksBeforeUpdate = portfolio1.stockList;
+            const titleBeforeUpdate = portfolio1.title;
+            const commentsBeforeUpdate = portfolio1.comments;
+            const idBeforeUpdate = portfolio1.id;
 
-            portfolio1.updatePortfolio();
-            expect(portfolio1).not.toEqual(portfolio2);
-            expect(portfolio1.stockList).not.toEqual({});
-            expect(portfolio1.totalValue).not.toEqual(0);
-            //We should be sure that all the stats of a stock are not 
-            //their intially - assigned values
-            expect(portfolio1.stockList['AMZN'].companyName).not.toEqual('');
-            expect(portfolio1.stockList['AMZN'].latestTime).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].primaryExchange).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].latestPrice).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].PERatio).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].week52High).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].week52Low).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].ytdChange).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].closePrice).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].openPrice).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].beta).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].dividendRate).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].dividendYield).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].latestEPS).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].latestEPSDate).not.toEqual(null);
-            expect(portfolio1.stockList['AMZN'].investmentAmount).not.toEqual(null);
-            //Because there are two stocks in stockList, we need to make sure they have both been updated
-            expect(portfolio1.stockList['GOOGL'].companyName).not.toEqual('');
-            expect(portfolio1.stockList['GOOGL'].latestTime).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].primaryExchange).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].latestPrice).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].PERatio).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].week52High).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].week52Low).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].ytdChange).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].closePrice).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].openPrice).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].beta).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].dividendRate).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].dividendYield).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].latestEPS).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].latestEPSDate).not.toEqual(null);
-            expect(portfolio1.stockList['GOOGL'].investmentAmount).not.toEqual(null);
 
-            expect(portfolio1.stockList['AMZN']).not.toEqual(portfolio1.stockList['GOOGL']);
+            await portfolio1.updatePortfolio();
 
+            expect(portfolio1.stockList).toEqual(stocksBeforeUpdate);
+            expect(portfolio1.title).toEqual(titleBeforeUpdate);
+            expect(portfolio1.comments).toEqual(commentsBeforeUpdate);
+            expect(portfolio1.id).toEqual(idBeforeUpdate);
+            expect(portfolio1.lastModified).not.toEqual(null);
+
+            for (let key in portfolio1.stockList['AMZN']){
+                expect(portfolio1.stockList['AMZN'][key]).not.toEqual(null);
+            }
+            for (let key in portfolio1.stockList['GOOGL']){
+                expect(portfolio1.stockList['GOOGL'][key]).not.toEqual(null);
+            }
+            
+            let googlValue = portfolio1.stockList['GOOGL'].investmentAmount;
+            let amznValue = portfolio1.stockList['AMZN'].investmentAmount;
+            expect(portfolio1.totalValue).toEqual(googlValue + amznValue);
+
+            //Ensure that multiple calls results in the correct behavior
+            await portfolio1.updatePortfolio();
+            googlValue = portfolio1.stockList['GOOGL'].investmentAmount;
+            amznValue = portfolio1.stockList['AMZN'].investmentAmount;
+            expect(portfolio1.totalValue).toEqual(googlValue + amznValue);
+            
         });
     });
 
