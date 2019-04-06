87d2d7fcbea8e0d5492923f696c5814c5c8de5fb
Kenny Jarnagin <jarnagink19@hanover.edu>
2019-04-05 20:31:09 -0400

    Updates

---
+++ precache-manifest.3275f27e80d7703bbd2eb05bedb4babe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aab35c3abf83504ad4e",
+    "url": "/kenjarna/NvestorGenie/static/js/runtime~main.9aab35c3.js"
+  },
+  {
+    "revision": "c210bc86eddf5649554e",
+    "url": "/kenjarna/NvestorGenie/static/js/main.c210bc86.chunk.js"
+  },
+  {
+    "revision": "d5df0ee899c964ca38d9",
+    "url": "/kenjarna/NvestorGenie/static/js/1.d5df0ee8.chunk.js"
+  },
+  {
+    "revision": "c210bc86eddf5649554e",
+    "url": "/kenjarna/NvestorGenie/static/css/main.77f047a5.chunk.css"
+  },
+  {
+    "revision": "00f21d8bc22a0702737eb6ad450c62e4",
+    "url": "/kenjarna/NvestorGenie/index.html"
+  }
+];
