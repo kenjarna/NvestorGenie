1b2996b55076ee83ec23cf279b02e108cc741210
Kenny Jarnagin <jarnagink19@hanover.edu>
2019-04-05 20:38:14 -0400

    Updates

---
+++ precache-manifest.3554445ee2b2f76c18a173ba6397fe3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f14714ac8733b90294d",
+    "url": "/NvestorGenie/static/js/runtime~main.1f14714a.js"
+  },
+  {
+    "revision": "2ee78d283498f9d24900",
+    "url": "/NvestorGenie/static/js/main.2ee78d28.chunk.js"
+  },
+  {
+    "revision": "d5df0ee899c964ca38d9",
+    "url": "/NvestorGenie/static/js/1.d5df0ee8.chunk.js"
+  },
+  {
+    "revision": "2ee78d283498f9d24900",
+    "url": "/NvestorGenie/static/css/main.77f047a5.chunk.css"
+  },
+  {
+    "revision": "73c6bc3e0bdbff9a506fd916641a3520",
+    "url": "/NvestorGenie/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kenjarna/NvestorGenie/precache-manifest.3275f27e80d7703bbd2eb05bedb4babe.js"
+  "/NvestorGenie/precache-manifest.3554445ee2b2f76c18a173ba6397fe3a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kenjarna/NvestorGenie/index.html", {
+workbox.routing.registerNavigationRoute("/NvestorGenie/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
